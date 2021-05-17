7937fc539526007df8e11f5a9b20883c96a95c7e
redcodemohammed <redcodemohammed@gmail.com>
2021-05-17 14:49:46 +0300

    updated user urls.py, change regex url to path

--- dsc_platform/user/urls.py
+++ dsc_platform/user/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
+from django.urls import path
 from .views import RegisterView, UserProfileView
 from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    url(r'^signup', RegisterView.as_view()),
-    url(r'^login', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^token/refresh/', jwt_views.TokenRefreshView.as_view(),
-        name='token_refresh'),
+    path('signup', RegisterView.as_view()),
+    path('login', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 
-    url(r'^user', UserProfileView.as_view())
+    path('user', UserProfileView.as_view())
 ]
